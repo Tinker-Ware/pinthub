9e834bdbe0cbd8da38a3b15a68926daebdba465b
Antonio Cabrera <antonio.cabreraglz@gmail.com>
2016-09-20 22:00:04 -0500

    Update mix file to add HTTPoison

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule PintHub.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger, :httpotion]]
+    [applications: [:logger, :httpoison]]
   end
 
   # Dependencies can be Hex packages:
@@ -27,8 +27,7 @@ defmodule PintHub.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [{:httpotion, "~> 3.0.0"},
-    {:poison, "~> 2.0"},
+    [{:poison, "~> 2.0"},
     {:httpoison, "~> 0.9.0"}]
   end
 end
